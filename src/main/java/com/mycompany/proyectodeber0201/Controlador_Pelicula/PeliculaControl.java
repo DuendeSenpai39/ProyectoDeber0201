f791becc74a244c13b78c4f7965f5e5d0f89bd9e
Jose <Jose@DESKTOP-G0V731S>
2022-11-14 20:45:44 -0500

    Terminado TGP

---
+++ src/main/java/com/mycompany/proyectodeber0201/Controlador_Pelicula/PeliculaControl.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectodeber0201.Controlador_Pelicula;
+
+/**
+ *
+ * @author Jose
+ */
+public class PeliculaControl {
+    
+}
