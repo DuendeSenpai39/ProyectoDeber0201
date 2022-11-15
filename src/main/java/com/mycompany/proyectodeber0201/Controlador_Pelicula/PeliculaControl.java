4ebff5de8a51532eebcbe1aca38438bdbd199c59
Jose <Jose@DESKTOP-G0V731S>
2022-11-14 23:03:52 -0500

    Terminado TGP

--- src/main/java/com/mycompany/proyectodeber0201/Controlador_Pelicula/PeliculaControl.java
+++ src/main/java/com/mycompany/proyectodeber0201/Controlador_Pelicula/PeliculaControl.java
@@ -41,7 +41,7 @@ public class PeliculaControl {
         retorno ="Pelicula: "+pelicula.getNombre()+" Director: "+pelicula.getDirector()+"\n"
                 +"Porcentaje de Aceptacion: "+pelicula.getPorcentajeAceptacion()+"\n"
                 +"Recaudacion de:"+pelicula.getRecaudacion()+" miles de millones de dolares \n"
-                +"Pelicula ano: "+pelicula.getYear()+ " a sido creado correctamente";
+                +"Pelicula de: : "+pelicula.getYear();
         return retorno;
     }
 
